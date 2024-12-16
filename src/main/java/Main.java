6e486dc15995b7dec1f6c96c1765fa6c9b7f675a
spatburgunder <neris_08@mail.ru>
2024-12-16 15:15:11 +0300

    add web-server and healthcheck

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import spark.Service;
 
 
 public class Main {
-    public static void main(String[] args) throws TelegramApiException {
+    public static void main(String[] args) throws TelegramApiException, InterruptedException {
         // Создаем экземпляр TelegramBotsApi
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
 
@@ -14,6 +15,16 @@ public class Main {
             System.out.println("Бот запущен");
         } catch (TelegramApiException e) {
             e.printStackTrace();
+        }
+
+        // Настройка healthcheck
+        Service httpService = Service.ignite();
+        httpService.port(8080); // Порт для healthcheck
+        httpService.get("/health", (req, res) -> "OK");
+
+        // Ждем завершения работы бота
+        while (true) {
+            Thread.sleep(Long.MAX_VALUE);
         }
     }
 }
